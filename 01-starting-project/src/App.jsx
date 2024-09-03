a78fc8e6246e6b39ea52e19a5fe819b90f8f4509
Ryan Lehan <RLehan@tql.com>
2024-09-03 13:12:19 -0400

    Different uses of 'props'

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,6 +1,7 @@
 // Images should be imported instead of directly referenced
 import reactImg from './assets/react-core-concepts.png';
 import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';            // Must use curly braces b/c it is a named export, not default
 
 // Randomize Dynamic text
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
@@ -49,9 +50,37 @@ function App() {
       <section id="core-concepts">
         <h2>Core Concepts</h2>
           <ul>
+            {/*  This is an example of explicit use of data for the properites/attributes of the CoreConcept tag
             <CoreConcept title="Components" description="The core UI building block" image={componentsImg} />
             <CoreConcept title="Props" description="Passing property values to function" image={componentsImg} />
             <CoreConcept title="JSX" description="Javascript Extension" image={componentsImg} />
+            */}
+            {/*  This is an example of using table driven data (from data.js) 
+            <CoreConcept 
+              title={CORE_CONCEPTS[0].title} 
+              description={CORE_CONCEPTS[0].description} 
+              image={CORE_CONCEPTS[0].image} 
+            />
+            <CoreConcept 
+              title={CORE_CONCEPTS[1].title} 
+              description={CORE_CONCEPTS[1].description} 
+              image={CORE_CONCEPTS[1].image} 
+            />
+            <CoreConcept 
+              title={CORE_CONCEPTS[2].title} 
+              description={CORE_CONCEPTS[2].description} 
+              image={CORE_CONCEPTS[2].image} 
+            />
+            <CoreConcept 
+              title={CORE_CONCEPTS[3].title} 
+              description={CORE_CONCEPTS[3].description} 
+              image={CORE_CONCEPTS[3].image} 
+            />
+            */}
+            {/* This is an example of using the map function*/}
+            {CORE_CONCEPTS.map((cc) => (
+              <CoreConcept title={cc.title} description={cc.description} image={cc.image} />
+            ))}
           </ul>
         </section>
       </main>
